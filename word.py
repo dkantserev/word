53f11d56bbae378596bd30ba0297a95a46eea4d1
Dmitriy_Kantserev <dkantserev@gmail.com>
2024-04-26 23:22:39 +0300

    Добавьте файлы проекта.

---
+++ word.py
@@ -0,0 +1,30 @@
+word = input()
+
+vowel = ['a','e','i','o','u']
+
+lenght = len(word)
+a = 0
+e = 0
+i = 0
+u = 0
+
+for j in range(lenght):
+    for letter in vowel:
+        if word.find(letter,j,j+1) != -1:
+            if letter == 'a':
+                a+=1
+            elif letter == 'e':
+                e+=1
+            elif letter == 'i':   
+                i+=1
+            elif letter == 'u':  
+                u+=1
+  
+if a == 0 or e == 0 or i == 0 or u == 0:
+    print("False")
+else:
+    print(" Слово содержит букву a",a,"раз, e",e,"раз, i",i,"раз, u",u,"раз.")
+    
+    
+    
+
